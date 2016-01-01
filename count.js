61d0b6b5bb149acacbc588809956983deb454cc9
isdaft <ben.ashby2@gmail.com>
2016-01-01 21:04:37 +0000

    last

---
+++ count.js
@@ -0,0 +1,14 @@
+var age = process.argv[2];
+var url = "mongodb://localhost:27017/learnyoumongo";
+var mongo = require('mongodb').MongoClient
+mongo.connect(url, function(err,db) {
+	if (err) throw err
+	var parrots = db.collection('parrots');
+	parrots.count({
+     	 age: { $gt: +age }
+    	}, function(err, count) {
+    	if (err) throw err
+	console.log(count);
+	db.close();
+    	})
+})
